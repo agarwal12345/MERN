52a8c07eaa3fc2508a688a80ae2c1c00beebcaa7
agarwal12345 <144110975+agarwal12345@users.noreply.github.com>
2023-10-02 12:21:27 +0530

    Add files via upload

---
+++ script/script4.js
@@ -0,0 +1,7 @@
+function sortString() {
+  const inputString = document.getElementById("inputString").value;
+  const sortedString = inputString.split("").sort().join("");
+  document.getElementById(
+    "result"
+  ).innerText = `Alphabetical Order: ${sortedString}`;
+}
